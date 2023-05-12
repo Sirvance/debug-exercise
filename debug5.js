9e4f0038bb25608cd1b5dfc8a6aab51ee3cf24a6
vance <vancedowdell2110@yahoo.com>
2023-05-11 19:13:27 -0700

    new

--- debug5.js
+++ debug5.js
@@ -8,7 +8,7 @@ function reverseString(str) {
     let reversedStr = "";
     
     for (let i = str.length - 1; i > 0; i--) {
-      reversedStr += i;
+      reversedStr += {str}(i);
     }
     return reversedStr;
   }
